4a8d1f5acecde2496dda0cbf543c0a28a3b55470
Rich Leonard <rich@homeimprovement-li.com>
2016-09-12 16:41:33 -0400

    A real mess so far...

--- js/app.js
+++ js/app.js
@@ -14,4 +14,37 @@ $(document).ready(function(){
 
 });
 
+	/*----  and so it begins ----*/
 
+
+             // ----- Start a New Game  -----
+	//  $('.new').on('click', function(event) {
+    //  location.reload()
+    //  console.log('you clicked new game');
+
+
+            // ---------  Generate random number 1 - 100  -------
+//   function generateNumber(startValue, endValue){ var iChoices = endValue - startValue + 1;
+// return Math.floor(Math.random() * iChoices + startValue);
+//   }
+// 	var iNum = generateNumber(0, 100);
+// 	console.log("Generated the number " + iNum);
+
+
+            // ---------  Get Input, compare, is it Hot || Cold ?  -------
+
+//   $('#userGuess').on('click', function(event){
+//     console.log("you clicked?");
+
+//   }
+
+
+// --------- ????  ----------------- some thoughts  ------
+// var input = $("#.userGuess"){
+// if (iNum = input){ 
+	// numMatch = ""
+// }
+
+// }
+
+//  });
