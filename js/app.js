bc0c5e289a14b02b331514ab0f27d785541d0059
Rich Leonard <rich@homeimprovement-li.com>
2016-09-22 14:33:50 -0400

    Recent update as of 9/22 2:35pm

--- js/app.js
+++ js/app.js
@@ -1,50 +1,19 @@
-
-$(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-
-  	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+$(".what").click(function() {
+    $(".overlay").fadeIn(1000);
 
 });
 
-	/*----  and so it begins ----*/
+$("a.close").click(function() {
+    $(".overlay").fadeOut(1000);
+});
 
+$("#userGuess").click(function(event){ 
+  event.preventDefault();
+  var UserInput = $("#userGuess").val(); 
+  console.log(UserInput); 
+});
 
-             // ----- Start a New Game  -----
-	//  $('.new').on('click', function(event) {
-    //  location.reload()
-    //  console.log('you clicked new game');
-
-
-            // ---------  Generate random number 1 - 100  -------
-//   function generateNumber(startValue, endValue){ var iChoices = endValue - startValue + 1;
-// return Math.floor(Math.random() * iChoices + startValue);
-//   }
-// 	var iNum = generateNumber(0, 100);
-// 	console.log("Generated the number " + iNum);
-
-
-            // ---------  Get Input, compare, is it Hot || Cold ?  -------
-
-//   $('#userGuess').on('click', function(event){
-//     console.log("you clicked?");
-
-//   }
-
-
-// --------- ????  ----------------- some thoughts  ------
-// var input = $("#.userGuess"){
-// if (iNum = input){ 
-	// numMatch = ""
-// }
-
-// }
-
-//  });
+// $(".new").click(function(){ 
+//   $("#form")[0].reset(); 
+// });
+ 
